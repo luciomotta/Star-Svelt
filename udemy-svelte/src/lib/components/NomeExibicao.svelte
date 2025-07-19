8cff3cd5b7e46e8b17b0f1f9158ca26bbbe009b8
Lucio <lucio-hto@hotmail.com>
2025-07-19 01:45:20 -0300

    ⚡️ feat: add NomeExibicao component and update main page layout

---
+++ udemy-svelte/src/lib/components/NomeExibicao.svelte
@@ -0,0 +1,25 @@
+<script lang="ts">
+    let fistName = $state('Lúcio');
+    let lastName = $state('Motta');
+    let fullName = $state(`${fistName} ${lastName}`);
+// URL válida para imagem de calculadora
+    let src = 'https://picsum.photos/250/300';
+    let bio = 'Olá, <b class="text-red-500">Mundo!</b> <a class="text-blue-600" href="https://www.udemy.com">Udemy</a>';
+</script>
+
+<div>
+    <h1>Hello {fistName}</h1>
+    <img src={src} alt={`foto do ${fistName}`} class="w-32 h-32">
+    <p>{@html bio}</p>
+</div>
+
+<style>
+    h1 {
+        color: blue;
+        font-size: 2em;
+    }
+</style>
+
+
+
+
